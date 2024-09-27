990cacb9e3f9b14572797fea672af4f79d305476
rjkozlowskijr <rjkozlowskijr@gmail.com>
2024-09-27 08:15:45 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>CSC 325 Community Service App</title>
+        <link rel="stylesheet" href="inc/Styles.css">
+    </head>
+    <body>
+        <?php include "inc/nav.php"; ?>
+        <main>
+            <section>
+                <h2>Featured Projects</h2>
+                <ul>
+                    <li>Local Food Drive - 2 PM on 9/28</li>
+                    <li>Heritage Park Cleanup - 11 AM on 9/21 </li>
+                    <li>Polar Plunge - Cedar Beach - 12 PM on 10/5</li>
+                </ul>
+            </section>
+            <section>
+                <h2>About Our Community</h2>
+                <p>Here you may sign up for projects as well as provide updates for projects in progress</p>
+            </section>
+        </main>
+        <?php include "inc/foot.php"; ?>
+    </body>
+</html>
