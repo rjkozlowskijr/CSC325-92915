f40f9d7714c9e1bc8547b9465279e05e06858c53
rjkozlowskijr <rjkozlowskijr@gmail.com>
2024-10-25 12:00:48 -0400

    user db, reg, session control

--- inc/nav.php
+++ inc/nav.php
@@ -2,5 +2,13 @@
     <h1>CSC 325 Community Service App</h1>
     <nav>        
         <a href="index.php">Home</a>
+        <?php
+            session_start();
+            if (!isset($_SESSION['username'])) {
+                echo "<a href='login.php'>Login</a>";
+            } else {
+                echo "<a href='logout.php'>Logout : " . $_SESSION['username'] . "</a>";
+            }
+        ?>
     </nav>
 </header>
